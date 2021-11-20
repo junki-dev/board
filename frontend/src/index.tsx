999a40db207ee2a43348048dab1e5c82145b12a5
Junki Kim <kjgcde@gmail.com>
2021-11-20 13:17:11 +0000

    feat: apollo client 설정

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,8 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ApolloClient, InMemoryCache, HttpLink, from, ApolloProvider } from '@apollo/client';
+import { onError } from '@apollo/client/link/error';
 import MainLayout from './components/MainLayout';
 import './assets/css/font.css';
 
 const rootElement = document.getElementById('root');
 
-ReactDOM.render(<MainLayout />, rootElement);
+const httpLink = new HttpLink({
+  uri: `${process.env.REACT_APP_BACKEND_URL}/graphql`,
+});
+
+const errorLink = onError(({ graphQLErrors, networkError }) => {
+  if (graphQLErrors)
+    graphQLErrors.forEach(({ message, locations, path }) =>
+      console.log(`[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`),
+    );
+
+  if (networkError) console.log(`[Network error]: ${networkError}`);
+});
+
+const client = new ApolloClient({
+  link: from([errorLink, httpLink]),
+  cache: new InMemoryCache(),
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <MainLayout />
+  </ApolloProvider>,
+  rootElement,
+);
